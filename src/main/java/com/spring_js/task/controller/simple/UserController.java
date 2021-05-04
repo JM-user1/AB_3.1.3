f00da21ba714fb87866599af4b5ad0ac7f0ab3f6
JM-user1 <80413442+JM-user1@users.noreply.github.com>
2021-05-04 20:19:55 +0700

    v.04

--- src/main/java/com/spring_js/task/controller/simple/UserController.java
+++ src/main/java/com/spring_js/task/controller/simple/UserController.java
@@ -1,7 +1,5 @@
 package com.spring_js.task.controller.simple;
 
-import com.spring_js.task.model.User;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @GetMapping("/userPage")
-    public String showUserForm(@AuthenticationPrincipal User user) {
+    public String showUserForm() {
         return "/user/userPage";
     }
 }
