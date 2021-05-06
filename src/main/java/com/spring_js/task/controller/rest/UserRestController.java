cec88a172d461e8afbf371fb353b130acc4626ff
JM-user1 <80413442+JM-user1@users.noreply.github.com>
2021-05-06 15:23:41 +0700

    v.05

--- src/main/java/com/spring_js/task/controller/rest/UserRestController.java
+++ src/main/java/com/spring_js/task/controller/rest/UserRestController.java
@@ -1,13 +1,11 @@
 package com.spring_js.task.controller.rest;
 
 import com.spring_js.task.model.User;
-import com.spring_js.task.repository.UserRepo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,19 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserRestController {
 
-    private final UserRepo userRepo;
-
-    public UserRestController(UserRepo userRepo) {
-        this.userRepo = userRepo;
-    }
-
     @GetMapping("/authUser")
     public ResponseEntity<User> getAuthUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         User user = new User();
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
-            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-            user = userRepo.findByName(userDetails.getUsername());
+            user = (User) authentication.getPrincipal();
         }
         return user != null
                 ? new ResponseEntity<>(user, HttpStatus.OK)
